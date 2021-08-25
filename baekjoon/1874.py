4651eb0f0f303b5e09e1b584b5d6bd3654b81bd8
mildroast <mildroast@naver.com>
2021-08-25 09:09:30 +0900

    210825 0905 아침 문제 풀이

--- baekjoon/1874.py
+++ baekjoon/1874.py
@@ -1,6 +1,23 @@
 from collections import deque
+s = deque()
+res = []
+cnt = 0
 
 n = int(input())
 
-for _ in range(n):
-    i = int(input())
+for i in range(n):
+    v = int(input())
+
+    while cnt < v:
+        cnt += 1
+        s.append(cnt)
+        res.append("+")
+
+    if s[-1] == v:
+        s.pop()
+        res.append("-")
+    else:
+        print("NO")
+        exit()
+
+print("\n".join(res))
